48395dfe8abb4466ed1368c725b60a9a672272e6
Dileep Rajput <dileep_rajput@yahoo.com>
2018-03-09 17:15:15 -0500

    Add dummy routes to register parties and users

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,1 +1,19 @@
-console.log('working');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express();
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/register_party', (req, res) => {
+  res.send({message: 'Party was successfully registered!'})
+})
+
+app.post('/register_guest', (req, res) => {
+  res.send({message: 'Guest was successfully registered!'})
+})
+
+app.listen(process.env.PORT || 8081)
