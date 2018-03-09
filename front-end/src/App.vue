20b869c0d9bec074165058fcc87592c7aff9583b
Dileep Rajput <dileep_rajput@yahoo.com>
2018-03-09 18:59:43 -0500

    Move App.vue stuff to Host.vue to configure App.vue for routes

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,65 +1,22 @@
 <template>
   <div id="app">
-    <search-bar v-on:newPlaylistItem="addItemToPlaylist" id="app-searchbar"></search-bar>
-    <playlist-body v-bind:room-playlist="getRoomPlaylist" id="app-playlistbody"></playlist-body>
-    <player-bar v-bind:room-playlist="getRoomPlaylist" id="app-playerbar"></player-bar>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import SearchBar from "./components/SearchBar"
-import PlaylistBody from "./components/PlaylistBody"
-import PlayerBar from "./components/PlayerBar"
-
 export default {
-  name: "App",
-  data () {
-    return {
-      roomPlaylist: []
-    }
-  },
-  components: {
-    SearchBar,
-    PlaylistBody,
-    PlayerBar
-  },
-  methods: {
-    addItemToPlaylist: function(videoItem) {
-      this.roomPlaylist.push(videoItem);
-    }
-  },
-  computed: {
-    getRoomPlaylist: function() {
-      return this.roomPlaylist;
-    }
-  }
+  name: 'app'
 }
 </script>
 
 <style>
-  body {
-    margin: 0;
-    padding: 0;
-    background-color: skyblue;
-  }
-
-  #app-searchbar {
-    position: fixed;
-    top: 0;
-    width: 100%;
-    height: 80px;
-    background-color: aliceblue;
-  }
-
-  #app-playlistbody {
-    margin-top: 100px;
-  }
-
-  #app-playerbar {
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    height: 80px;
-    background-color: white;
-  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
