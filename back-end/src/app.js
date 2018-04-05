a64b1fa946e1d6f75e6a97fafc6e1b6ead11bf64
Dileep Rajput <dileep_rajput@yahoo.com>
2018-04-05 13:13:42 -0400

    Add comments to clarify code in app.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,86 +3,126 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 
-var mongoose = require('mongoose');
+//connect to database
+const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost');
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function(callback){
   console.log("Connection Succeeded");
 });
 
-var Party = require('../models/Party');
+const Party = require('../models/Party');
 
+//create backend app
 const app = express();
-
 const server = app.listen(process.env.PORT || 8081);
+
+//create websockets
 const io = require('socket.io').listen(server);
-
-
 const socketEventHandler = require('./socketevent-handler');
 
+//setup middleware
 // app.use(morgan('combined'))
 app.use(bodyParser.json());
 app.use(cors());
 
+/**
+ * @summary Creates a party with the provided username and password.
+ * @description
+ * Accepts post requests on the /register_party route. Expects a json with the
+   following data:
+   {
+      partyName: string,
+      partyPassword: string
+   }
+   On success it will send back the party name. Otherwise it will send a json
+   with an error message in it. ex: { error: string }
+ * @listens Server#register_party_route
+ * @emits JSON
+ */
 app.post('/register_party', (req, res) => {
   var name = req.body.partyName;
   var password = req.body.partyPassword;
 
+  //check if both name and password fields are not empty
   if(!name || !password) {
     res.status(400).send({error : "Must provide a json containing a partyName and partyPassword"});
     return;
   }
 
+  //look up provided party name
   var result = Party.find({partyName: name}).exec((err, party) => {
     if (err) {
       res.status(500).send({error: "Encountered database error while checking if party exists"});
       return;
     }
 
+    //return error if party with that name already exists
     if (party.length != 0){
       res.status(200).send({error: "A party with that name already exists!"});
       return;
     }
 
+    //otherwise create new party object to store in db
     var newParty = new Party({
       partyName: name,
       partyPassword: password,
       guests: [[]]
     })
 
+    //save newly created party object
     newParty.save((err, newParty) => {
       if (err) {
         res.status(500).send({error: "Encountered database error while checking if party exists"});
         return;
       }
 
+      //send back partyname to indicate success
       res.status(200).send({partyName: name});
     })
 
   })
 })
 
+/**
+ * @summary registers a guest with the provided username and password.
+ * @description
+ * Accepts post requests on the /register_guest route. Expects a json with the
+   following data:
+   {
+      partyName: string,
+      partyPassword: string
+   }
+   On success it will send back the party name and the guests guestID. Otherwise
+   it will send a json with an error message in it. ex: { error: string }
+ * @listens Server#register_guest_route
+ * @emits JSON
+ */
 app.post('/register_guest', (req, res) => {
   var name = req.body.partyName;
   var password = req.body.partyPassword;
 
+  //check if both name and password fields are not empty
   if(!name || !password) {
     res.status(400).send({error : "Must provide a json containing a partyName and partyPassword"});
     return;
   }
 
+  //validate that there is a party with that name and password
   var result = Party.findOne({partyName: name, partyPassword: password}).exec((err, party) => {
     if (err) {
       res.status(500).send({error: "Encountered database error while checking if party exists"});
       return;
     }
 
+    //if not, then tell the user they provided invalid creditenals
     if (!party){
       res.status(200).send({error: "Invalid partyName or partyPassword"});
       return;
     }
 
+    //otherwise add guest data to the database
     party.guests.push([])
     party.save((err, result) => {
       if (err) {
@@ -90,11 +130,13 @@ app.post('/register_guest', (req, res) => {
         return;
       }
 
+      //send back json with partyName and the registered guests guestID.
       res.status(200).send({PartName: name, guestID: result.guests.length - 1});
     })
   })
 })
 
+//enable socket-io
 io.on("connection", function(socket) {
   console.log("Connected");
   socketEventHandler.handle(socket);
