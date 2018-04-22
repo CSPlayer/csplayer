9bce5851b21aa20423626d43618445bee955deae
Hugh <hugh.j.wu@gmail.com>
2018-04-21 21:35:21 -0400

    Updated CSS code for LandingPage and globalstyles

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -15,11 +15,7 @@
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
     background-color: #222222;
     height: 100%;
   }
-
-
-
 </style>
--- front-end/src/LandingPage.vue
+++ front-end/src/LandingPage.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="landing-page">
+  <div id="landing-page" class="center">
 
-    <div id="circles-container" class="page-center">
-      <img id="circle-outer" class="circles" src="./assets/circle-outer.svg">
-      <img id="circle-inner" class="circles" src="./assets/circle-inner.svg">
+    <div>
+      <img id="circle-outer" src="./assets/circle-outer.svg">
+      <img id="circle-inner" src="./assets/circle-inner.svg">
     </div>
 
-    <div class="page-center">
+    <div class="form-container">
       <h1>CSPlayer</h1>
       <form>
         <label for="partyName">Party Name</label> <br>
@@ -14,9 +14,10 @@
 
         <label for="partyPassword">Party Password</label> <br>
         <input name="partyPassword" type="password" v-model="partyPassword">
+        <br>
+        <button v-on:click="HostParty">Host a party</button>
+        <button v-on:click="JoinParty">Join a party</button>
       </form>
-      <button v-on:click="HostParty">Host a party</button>
-      <button v-on:click="JoinParty">Join a party</button>
       <div style="color: red;">
         {{ error }}
       </div>
@@ -73,20 +74,25 @@ export default {
     height: 100%;
     color: #e3e3e3;
   }
- 
+
+  .form-container {
+    position: relative;
+    z-index: 3;
+  }
+
   h1 {
     font-size: 8em;
   }
 
   form {
-    margin: 20px;
+    text-align: center;
     font-size: 1.2em;
   }
 
-  form input {
-    margin: 14px;
+  input {
+    margin: 15px;
     text-align: center;
-    background-color: rgba(34, 34, 34, 0.0);
+    background-color: rgba(22, 22, 22, 0.0);
     border-width: 0 0 1px 0;
     border-color: #e3e3e3;
     color: #e3e3e3;
@@ -96,32 +102,25 @@ export default {
     border: none;
     border-radius: 10%;
     padding: 10px;
-    margin: auto 10px;
-  }
-
-  .page-center {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+    margin: 20px;
   }
 
   /** 
    * All of this below is just to get goddamn spinning circles
    */
-  .circles {
-    position: absolute;
-    left: -330px;
-    top: -330px;
-  }
-
   #circle-inner {
-    z-index: 5;
+    position: fixed;
+    left: calc(50% - 330px);
+    top: calc(50% - 330px);
+    z-index: 2;
     animation: spin-clockwise 60s linear infinite;
   }
 
   #circle-outer {
-    z-index: 6;
+    position: fixed;
+    left: calc(50% - 330px);
+    top: calc(50% - 330px);
+    z-index: 1;
     animation: spin-counterclockwise 70s linear infinite;
   }
 
@@ -140,6 +139,20 @@ export default {
     }
     to {
       transform: rotate(-360deg);
+    }
+  }
+
+  /* Remove the spinning circles on mobile devices */
+  @media screen and (max-width: 768px) {
+    h1 {
+      text-align: center;
+      font-size: 5em;
+      margin: 0 auto 40px;
+      /* margin: 50px; */
+    }
+    
+    #circle-inner, #circle-outer {
+      display: none !important;
     }
   }
 </style>
